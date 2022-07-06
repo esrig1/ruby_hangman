5ad612096491abad2a3f5d5433e142ca5e58290d
esrig <joshua.esrig@gmail.com>
2022-07-06 16:17:16 -0700

    game functionality complete, save functionality being added soon

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,5 @@
+
+
 class Game 
 
     def initialize(word = [], curr_guesses = [], guesses_left = 0)
@@ -7,19 +9,58 @@ class Game
     end
 
     def is_winner? 
-        if word == curr_guesses
+        if @word == @curr_guesses
             true
         else
             false
         end
     end
 
-    def getLetter
-        puts "Enter a letter as your guess, you currently have #{@guesses_left} guess(es) left"
-        guess = gets.chomp 
+    def guesses_left
+        return @guesses_left
     end
+    
+    def display_board 
+        @curr_guesses.each do |letter| 
+            print "#{letter} "
+        end
+        puts
+    end
+
+    def get_letter
+        correct_guess = false
+        puts "Enter a letter as your guess, you currently have #{@guesses_left} wrong guess(es) left"
+        guess = gets.chomp.downcase
+        
+        @word.each_with_index do |char, index|
+            if char.eql?(guess)
+                @curr_guesses[index] = guess.to_str
+                correct_guess = true
+            end
+        end
+        display_board
+        if not correct_guess
+            @guesses_left -= 1
+        end
+    end
+
+end
+
+def execute_game(game_object)
+    while !game_object.is_winner? && game_object.guesses_left > 0
+        game_object.get_letter
+    end
+    puts "end"
+end
+
+def save
+
 end
 
 
+word = ["p", "e", "n", "i", "s"]
+empty = ["_","_","_","_","_"]
+example = Game.new(word, empty, 5)
+execute_game(example)
 
-def get
+
